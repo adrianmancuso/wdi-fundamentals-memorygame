1059a5ca78a92134b7eed76fcb9fcf14ef384f72
adrianmancusp <adrian.mancuso@gmail.com>
2017-05-08 21:49:04 +1000

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "king";
+var cardFour = "queen";
+
+
+console.log("User flipped a " + cardFour);
+console.log("User flipped a " + cardTwo);
