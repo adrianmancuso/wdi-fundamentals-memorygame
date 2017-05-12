f4ff3a4bca2fe9ae15d3d02bae27e4d0407ca4e4
adrianmancusp <adrian.mancuso@gmail.com>
2017-05-12 11:53:47 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,29 @@
+//basic test for functionality
 console.log("Up and running!");
 
+//global variables
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped a " + cardsInPlay[0]);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped a " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
+//function which checks for matching cards and alerts
+//user if they were successful
+var checkForMatch = function (){
 	if (cardsInPlay[0] === cardsInPlay [1]) {
-		alert("You found a match! You win!");
-	} else {
-		alert("Sorry, try again :(");
+			alert("You found a match! You win!");
+		} else {
+			alert("Sorry, try again :(");
+		}
+}
+
+//function which logs cards in play to console
+var flipCard = function (cardId) {
+	console.log("User flipped a " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch ();
 	}
 }
+
+flipCard(0);
+flipCard(2);
