9e343a24d64616263f131e8577489870eede47f7
adrianmancusp <adrian.mancuso@gmail.com>
2017-05-14 14:48:18 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,6 +27,39 @@ var cards = [
 ];
 var cardsInPlay = [];
 
+
+
+//function which logs cards in play to console
+var flipCard = function () {
+	
+	var cardId = this.getAttribute('data-id');
+
+	console.log("User flipped a " + cards[cardId].rank);
+	console.log("The suit is " + cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch ();
+	}
+}
+
+//function to create game board
+var createBoard = function () {
+	for (var i = 0; i <cards.length; i++){
+		
+		var cardElement = document.createElement('img');
+
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+
+		document.getElementById('game-board').appendChild(cardElement)[i];
+	}
+};
+
 //function which checks for matching cards and alerts
 //user if they were successful
 var checkForMatch = function (){
@@ -37,17 +70,6 @@ var checkForMatch = function (){
 		}
 }
 
-//function which logs cards in play to console
-var flipCard = function (cardId) {
-	console.log("User flipped a " + cards[cardId].rank);
-	console.log("The suit is " + cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
+createBoard();
 
-	if (cardsInPlay.length === 2) {
-		checkForMatch ();
-	}
-}
 
-flipCard(0);
-flipCard(2);
